176d04c64e430f7edc38c3c9cab5a1dd2ec93749
Scott Gerring <scottgerring@users.noreply.github.com>
2023-03-03 18:27:15 +0100

    [chore] Update dependencies (#4)
    * Roll deps forward
    Co-authored-by: Scott Gerring <gerrings@amazon.com>

--- src/cloudformation_helpers.rs
+++ src/cloudformation_helpers.rs
@@ -185,9 +185,8 @@ pub async fn update_stack(
     match update_stack_response {
         Ok(_) => Ok(()),
         Err(e) => {
-            // TODO - when the SDK supports parsing this out properly, do that, rather than string matching.
-            // https://github.com/awslabs/aws-sdk-rust/issues/678
-            return if e.to_string().contains("No updates are to be performed.") {
+            let error_txt = format!("{:?}", e);
+            return if error_txt.to_string().contains("No updates are to be performed.") {
                 Ok(())
             } else {
                 Err(e)
